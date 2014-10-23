d52249ed1239525a4cd8fa6cd0d47d44311d34b0
Alex Gutteridge <alex.gutteridge@pfizer.com>
2014-10-23 12:34:38 +0100

    Missing ;

--- scripts/variant_effect_predictor/filter_vep.pl
+++ scripts/variant_effect_predictor/filter_vep.pl
@@ -577,8 +577,8 @@ sub filter_is_child {
     # connect to DBs here
     if(!defined($config->{ontology_adaptor})) {
       eval {
-	    require Bio::EnsEMBL::Registry;
-	  }
+        require Bio::EnsEMBL::Registry;
+      };
       
       if($@) {
         die("ERROR: Could not load Ensembl API modules\n");
